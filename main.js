cce6aa66504572bdf952125337cc1f568bda2bbb
taey1344 <xodus1344@gmail.com>
2020-08-19 22:59:29 +0900

    Finish app

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const adviceText = document.querySelector('.text');
 const btn = document.querySelector('.btn');
+const adviceArea = document.getElementById('advice-area');
 
 btn.addEventListener('click', advice);
+
 function advice() {
   fetch("https://api.adviceslip.com/advice")
     .then(response => response.json())
     .then(data => {
+      adviceArea.style.display = 'block';
       adviceText.innerHTML = data.slip.advice;
     })
     .catch(err => console.log(err));
